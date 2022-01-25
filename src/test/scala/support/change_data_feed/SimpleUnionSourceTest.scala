e1f94d64cfa1b5b441d42a27cda6c9860d1734df
Dima Oblamskyi <dmytro.oblamskyi@globallogic.com>
2022-01-25 16:21:23 +0200

    add test for SimpleUnion.queryAll

--- src/test/scala/support/change_data_feed/SimpleUnionSourceTest.scala
+++ src/test/scala/support/change_data_feed/SimpleUnionSourceTest.scala
@@ -2,6 +2,8 @@ package com.cxi.cdp.data_processing
 package support.change_data_feed
 
 import com.cxi.cdp.data_processing.support.BaseSparkBatchJobTest
+import com.cxi.cdp.data_processing.support.change_data_feed.ChangeDataQueryResult.TableMetadata
+import org.apache.spark.sql.DataFrameReader
 import org.mockito.Mockito._
 import org.scalatest.{Matchers, OptionValues}
 
@@ -19,11 +21,11 @@ class SimpleUnionSourceTest extends BaseSparkBatchJobTest with Matchers with Opt
         val cdfService = mock(classOf[ChangeDataFeedService])
 
         val firstTableDF = spark.createDataset(Seq(Row("Alice", 25))).toDF
-        val firstTableMetadata = ChangeDataQueryResult.TableMetadata("firstTable", 0L, 2L)
+        val firstTableMetadata = TableMetadata("firstTable", 0L, 2L)
         val firstTableChangeData = ChangeDataQueryResult(consumerId, Seq(firstTableMetadata), Some(firstTableDF))
 
         val secondTableDF = spark.createDataset(Seq(Row("Bob", 33), Row("Chris", 45))).toDF
-        val secondTableMetadata = ChangeDataQueryResult.TableMetadata("secondTable", 2L, 3L)
+        val secondTableMetadata = TableMetadata("secondTable", 2L, 3L)
         val secondTableChangeData = ChangeDataQueryResult(consumerId, Seq(secondTableMetadata), Some(secondTableDF))
 
         doReturn(firstTableDF, Seq.empty: _*).when(sparkSpy).table("firstTable")
@@ -51,11 +53,11 @@ class SimpleUnionSourceTest extends BaseSparkBatchJobTest with Matchers with Opt
         val cdfService = mock(classOf[ChangeDataFeedService])
 
         val firstTableDF = spark.createDataset(Seq(Row("Alice", 25))).toDF
-        val firstTableMetadata = ChangeDataQueryResult.TableMetadata("firstTable", 0L, 2L)
+        val firstTableMetadata = TableMetadata("firstTable", 0L, 2L)
         val firstTableChangeData = ChangeDataQueryResult(consumerId, Seq(firstTableMetadata), Some(firstTableDF))
 
         val secondTableDF = spark.createDataset(Seq(AnotherRow(33))).toDF
-        val secondTableMetadata = ChangeDataQueryResult.TableMetadata("secondTable", 2L, 3L)
+        val secondTableMetadata = TableMetadata("secondTable", 2L, 3L)
         val secondTableChangeData = ChangeDataQueryResult(consumerId, Seq(secondTableMetadata), Some(secondTableDF))
 
         doReturn(firstTableDF, Seq.empty: _*).when(sparkSpy).table("firstTable")
@@ -71,6 +73,46 @@ class SimpleUnionSourceTest extends BaseSparkBatchJobTest with Matchers with Opt
         }
     }
 
+    test("SimpleUnion.queryAll when schemas are compatible") {
+        val sparkSpy = spy(spark)
+
+        val firstTableName = "firstTable"
+        val firstTableDF = spark.createDataset(Seq(Row("Alice", 25))).toDF
+        val firstTableLatestVersion = 2L
+
+        val secondTableName = "secondTable"
+        val secondTableDF = spark.createDataset(Seq(Row("Bob", 33), Row("Chris", 45))).toDF
+        val secondTableLatestVersion = 5L
+
+        val dataFrameReader = mock(classOf[DataFrameReader])
+        doReturn(dataFrameReader, Seq.empty: _*).when(sparkSpy).read
+        doReturn(dataFrameReader, Seq.empty: _*).when(dataFrameReader).format("delta")
+        doReturn(dataFrameReader, Seq.empty: _*).when(dataFrameReader).option("versionAsOf", firstTableLatestVersion)
+        doReturn(dataFrameReader, Seq.empty: _*).when(dataFrameReader).option("versionAsOf", secondTableLatestVersion)
+        doReturn(firstTableDF, Seq.empty: _*).when(dataFrameReader).table(firstTableName)
+        doReturn(secondTableDF, Seq.empty: _*).when(dataFrameReader).table(secondTableName)
+
+        val cdfService = mock(classOf[ChangeDataFeedService])
+        doReturn(firstTableLatestVersion, Seq.empty: _*)
+            .when(cdfService).getLatestAvailableVersion(firstTableName)(sparkSpy)
+        doReturn(secondTableLatestVersion, Seq.empty: _*)
+            .when(cdfService).getLatestAvailableVersion(secondTableName)(sparkSpy)
+
+        val cdfSource = new ChangeDataFeedSource.SimpleUnion(cdfService, Seq(firstTable, secondTable))
+
+        val expectedResult = ChangeDataQueryResult(
+            consumerId,
+            Seq(
+                TableMetadata(firstTableName, 0L, firstTableLatestVersion),
+                TableMetadata(secondTableName, 0L, secondTableLatestVersion)),
+            Some(firstTableDF.union(secondTableDF)))
+
+        val actualResult = cdfSource.queryAllData(consumerId)(sparkSpy)
+
+        actualResult.consumerId shouldBe expectedResult.consumerId
+        actualResult.tableMetadataSeq shouldBe expectedResult.tableMetadataSeq
+        actualResult.data.value.collect should contain theSameElementsAs expectedResult.data.value.collect
+    }
 }
 
 object SimpleUnionSourceTest {
